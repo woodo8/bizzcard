1b3143af4b156a251a7c52dcb0bdcd16d94613f8
woodo8 <yusufzodadovud33@gmail.com>
2023-02-08 03:25:52 +0300

    Added foo to the bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
-
+// import {BrowserRouter} from "react-router-dom"
 function App() {
+  const [first, setfirst] = useState("hello")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <BrowserRouter>
+      <div className="App">
+      <h1>hello</h1>
+      </div>
+    // </BrowserRouter>
   );
 }
 
